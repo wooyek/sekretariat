cf2f26c8e6712927df9eb72b7ee0e45caabd1fd0
Janusz Skonieczny <git.js@bravelabs.pl>
2019-03-15 22:19:01 +0100

    Update __date__

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -9,7 +9,7 @@ __credits__ = ["""Janusz Skonieczny"""]
 __version__ = '0.1.2'
 __status__ = "Alpha"
 __license__ = "Proprietary"
-__date__ = '2019-03-15 22:15'
+__date__ = '2019-03-15 22:19'
 
 
 def get_strict_version():
