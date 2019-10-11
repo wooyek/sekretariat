ab9937655c2d8a272b9e100f73624a451c92dd5b
Janusz Skonieczny <git.js@bravelabs.pl>
2019-10-11 19:19:17 +0200

    cache decisions

--- src/budget/views.py
+++ src/budget/views.py
@@ -141,7 +141,7 @@ class ApplicationListUser(AbstractAuthorizedView, FilterViewMixin, PaginationMix
 
 class ApplicationList(PermissionRequiredView, FilterViewMixin, PaginationMixin, ListView):
     model = models.Application
-    ordering = 'date'
+    ordering = '-submitted'
     paginate_by = 25
     permission_required = 'budget.view_application'
 
