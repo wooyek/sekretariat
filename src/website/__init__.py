86e600c260f194f1fa89905cfa3477d6891af572
Janusz Skonieczny <git.js@bravelabs.pl>
2019-10-15 17:36:38 +0200

    Bump version: 0.1.65 → 0.1.66

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -6,10 +6,10 @@ __email__ = 'js+pypi@bravelabs.pl'
 __copyright__ = "Copyright 2018, Janusz Skonieczny"
 __maintainer__ = """Janusz Skonieczny"""
 __credits__ = ["""Janusz Skonieczny"""]
-__version__ = '0.1.65'
+__version__ = '0.1.66'
 __status__ = "Alpha"
 __license__ = "Proprietary"
-__date__ = '2019-10-15 16:57'
+__date__ = '2019-10-15 17:36'
 
 
 def get_strict_version():
